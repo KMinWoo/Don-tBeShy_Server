aadc4f1f17ae0a71e23338ffe23c637b84d4963b
kmw811 <kmw811@naver.com>
2019-06-27 18:52:00 +0900

    #register method

--- database.js
+++ database.js
@@ -9,33 +9,67 @@ database.connection = mysql.createConnection({
   database: 'db',
   charset: 'utf8'
 });
-
 database.sql = [
   `select time, state from coupledate join client on coupledate.number = client.couplenumber where id = ? && (time >= ? && time < ?)`,
- ];
+  `insert into client(id, password, brithday, gender, couplenumber) values(?,?,?,false,0)`,
+  `insert into client(id, password, brithday, gender, couplenumber) values(?,?,?,true,0)`,
+  `select id from client where id = ?`,
+  `select coupleinfo.first as id_1, coupleinfo.second as id_2, couplestate.first, couplestate.second from coupleinfo join couplestate on coupleinfo.number = couplestate.number where coupleinfo.first = ? || coupleinfo.second=?`
+];
 
-// ` select * from coupledate join client on coupledate.number = client.couplenumber where id = ? && (time >= '2019-06' && time <'2019-07')`
-database.db_connection = function() {
+database.db_connection = function () {
   this.connection.connect();
 };
 
-database.getInfo = function(type,params,res) {
-  this.connection.query(this.sql[type], params, function (err, rows, fields) {
-    if (!err){
-      var arr = new Array();
-      var json = new Object();
-      for(var i = 0; i < rows.length; i++){ 
-        json.date = rows[i].time;
-        json.state = rows[i].state;
-        arr.push(json)
-      }
-      res.send(arr);
+database.idCheck = function (type, params, res) {
+  this.connection.query(this.sql[type], [params.id, params.id], function (err, rows, fields) {
+    if (!err) {
+      if (rows.length === 0)
+        res.send('true');
+      else
+        res.send('false');
+
     } else
       console.log('Error while performing Query.', err);
   });
 };
 
-database.end = function(){
+database.getInfo = function (type, params, res) {
+  this.connection.query(this.sql[type], params, function (err, rows, fields) {
+    if (!err) {
+      var arr = new Array();
+      for (var i = 0; i < rows.length; i++) {
+        var json = new Object();
+        json.date = rows[i].time;
+        json.state = rows[i].state;
+        arr.push(json)
+      }
+      res.send(arr);
+    } else console.log('Error while performing Query.', err);
+  });
+};
+
+database.getBtn = function (type, params, res) {
+  this.connection.query(this.sql[type], params, function (err, rows, fields) {
+    if (!err) {
+      var json = new Object();
+      if(rows[0].second == 1 && rows[0].first == 1) json.state = 2;
+      else json.state = rows[0].id_1 === params[0] ? rows[0].second : rows[0].id_2 === params[0] ? rows[0].first : 0;
+      res.send(json);
+    } else console.log('Error while performing Query.', err);
+  });
+};
+
+
+database.insertInfo = function (type, params) {
+  this.connection.query(this.sql[type], params, function (err, rows, fields) {
+    if (err) {
+      console.log('Error while performing Query.', err);
+    }
+  });
+};
+
+database.end = function () {
   this.connection.end();
 };
 
--- main.js
+++ main.js
@@ -11,33 +11,71 @@ app.set('views', './views');// defalut
 app.use(bodyParser.urlencoded({extended:false}))// post methd 데이터를 가져오기 위한 set 
 
 
+app.get('/id', (req, res)=>{
+    var id = req.query.id;
+    db.idCheck(3, id, res);
+});
 app.get('/register', (req, res)=>{
-    var id = req.body.id;
+    var id = req.query.id;
+    var password = req.query.password;
+    var brithdate = req.query.brithdate;
+    var gender = req.query.gender;
+    db.insertInfo(gender == 1 ? 1 : 2, [id, password, brithdate]);
+    res.send('check');
+});
 
-    var password = req.body.password;
-    var brithdate = req.body.brithdate;
-    var gender = req.body.gender;
+app.get('/couple', (req, res)=>{
+    var id = req.query.id;
+    var partner = req.query.partner;
 
-    db.insertClient();
 });
 
 
 app.get('/calendar', (req, res)=>{
-    //var id = req.body.id;
-    //var date = req.body.date;
-    // db(id, date);
-    
-    db.db_connection();
-    db.getInfo(0, ['kmw811', '2019-06', '2019-07'], res);
+    var id = req.query.id;
+    var year = req.query.year;
+    var month = req.query.month;
+    var date = '';
+    var nextDate ='';
+    if(month == 12)
+    {
+        nextDate = (year) + '-01';
+        year++;
+        date = year + '-' + month;
+    }else {
+        if(month < 10){
+            date = year + '-0' + month;
+        }else{
+            date = year + '-' + month;
+        }
+        month++;
+        if(month < 9){
+            nextDate = year + '-0' + month;
+        }else{
+            nextDate = year + '-' + month;
+        }
+    }
+    db.getInfo(0, [id, date, nextDate], res);
+});
+
+
+app.get('/btn', (req, res)=>{
+    var id = req.query.id;
+    db.getBtn(4, [id, id], res);
+});
+
+app.get('/button', (req, res)=>{
+    var id = req.query.id;
+    var year = req.query.year;
+    var month = req.query.month;
+    var day = req.query.day;
 
 });
 
 
 
 
-
-
+db.db_connection();
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
-
 });
